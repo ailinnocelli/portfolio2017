8fab1ea674fd61c58532fd5513dc77bec3e9c67e
ailinnocelli <ailin.nocelli@gmail.com>
2017-02-23 21:34:49 -0300

    Agrego galerias y campos de seccion skills

--- js/detail.js
+++ js/detail.js
@@ -1,4 +1,15 @@
-    $(document).ready(function(){
-      $('.parallax').parallax();
-      $('.carousel').carousel();
-    });
+$(document).ready(function(){
+	$('.parallax').parallax();
+	$('.carousel').carousel();
+});
+
+// Next slide
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3); // Move next n times.
+// Previous slide
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4); // Move prev n times.
+// Set to nth slide
+$('.carousel').carousel('set', 4);
+
+ 
